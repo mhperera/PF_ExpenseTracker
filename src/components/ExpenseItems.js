f81f6b8ecb4fd52d82a14f9f5f55a921dc80b86a
zmhp <manashahp1995@gmail.com>
2023-05-26 22:12:03 +0530

    Add ExpenseItems component

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseItems = () => {
+  return (
+    <div class="ExpenseItems">
+        <h1 className=''>Expense Items</h1>
+    </div>
+  )
+}
+
+export default ExpenseItems
