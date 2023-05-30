68acd183d2a61945420e52dec145bb78c27bb830
zmhp <manashahp1995@gmail.com>
2023-05-31 01:38:55 +0530

    Sert button to update expense title

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,16 +1,25 @@
 import ExpenseDate from './ExpenseDate';
 import Card from './../UI/Card';
+import React, { useState } from 'react';
 
 const ExpenseItems = (props) => {
+
+  const [ title, setTitle ] = useState(props.title);
+
+  const handleClick = () => {
+    setTitle('Updated Title');
+  }
+
 	return (
 		<Card className="ExpenseItems">
       <ExpenseDate
         date={props.date}
       />
 			<div className='ExpenseItems__description'>
-        <h2 className='ExpenseItems__title'>{ props.title }</h2>
+        <h2 className='ExpenseItems__title'>{ title }</h2>
         <div className='ExpenseItems__price'>LKR : { props.amount }</div>
       </div>
+      <button onClick={handleClick}>Change Title</button>
 		</Card>
 	);
 };
